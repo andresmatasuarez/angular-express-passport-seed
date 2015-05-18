de3647a9b3ee42833a7fb9dc5e352c630af18894
Andrés Mata Suárez <andresmata@zauberlabs.com>
2015-05-18 11:22:11 -0300

    Added XSRF protection using ExpressJS's CSURF module

--- server/app/app_config.js
+++ server/app/app_config.js
@@ -7,6 +7,7 @@ var compression  = require('compression');
 var session      = require('express-session');
 var bodyparser   = require('body-parser');
 var cookieparser = require('cookie-parser');
+var csurf        = require('csurf');
 var MongoStore   = require('connect-mongo')(session);
 var mongoose     = require('mongoose');
 var User         = require('../model/user');
@@ -24,17 +25,31 @@ exports.applyTo = function(app){
   app.use(bodyparser.json());
   app.use(cookieparser());
   app.use(session({
-    secret: config.server.session_secret,
-    saveUninitialized: true,
-    resave: true,
-    store: new MongoStore({
-      mongoose_connection: mongoose.connections[0]
-    })
+    secret            : config.server.session_secret,
+    saveUninitialized : true,
+    resave            : true,
+    cookie            : { secure: true },
+    store             : new MongoStore({ mongoose_connection: mongoose.connections[0] })
   }));
 
   // Passport
   app.use(passport.initialize());
   app.use(passport.session());
+
+  /*
+   * XSRF protection | Integration with AngularJS
+   * From official docs (https://docs.angularjs.org/api/ng/service/$http):
+   *   "[...] Angular provides a mechanism to counter XSRF. When performing XHR requests,
+   *    the $http service reads a token from a cookie (by default, XSRF-TOKEN) and sets
+   *    it as an HTTP header (X-XSRF-TOKEN)."
+   *
+   * Ideas for integration taken from: http://charandeepmatta.com/2014/06/04/csrfxsrf-protection-express-angular/
+   */
+  app.use(csurf());
+  app.use(function(req, res, next) {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    next();
+  });
 
   passport.use(User.createStrategy());
   passport.serializeUser(User.serializeUser());
