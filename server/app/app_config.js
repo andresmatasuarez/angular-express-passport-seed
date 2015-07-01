a8915da5c922affb36f98b5499cf8117c805b629
Andrés Mata Suárez <andresmata@zauberlabs.com>
2015-07-01 09:16:25 -0300

    Added favicon middleware

--- server/app/app_config.js
+++ server/app/app_config.js
@@ -1,10 +1,12 @@
 'use strict';
 
+var path        = require('path');
 var config      = require('config');
 var passport    = require('passport');
 var morgan      = require('morgan');
 var compression = require('compression');
 var bodyparser  = require('body-parser');
+var favicon     = require('serve-favicon');
 var Middlewares = require('../middlewares');
 var User        = require('../model/user');
 
@@ -16,6 +18,15 @@ exports.applyTo = function(app){
 
   if(config.env !== config.environments.test){
     app.use(morgan('dev'));
+  }
+
+  if (config.app.favicon){
+    app.use(favicon(path.join(__dirname, config.app.favicon)));
+  } else {
+    app.use('favicon.ico', function(req, res){
+      res.status(200);
+      res.type('image/x-icon');
+    });
   }
 
   app.use(compression());
