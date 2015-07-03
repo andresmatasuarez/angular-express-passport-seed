2102d82c2d686de0064f58807a6aacd0c3299715
Andrés Mata Suárez <andresmata@zauberlabs.com>
2015-07-02 21:08:03 -0300

    Added server.ssl.port=3001 to development

--- server/config/development.js
+++ server/config/development.js
@@ -5,7 +5,10 @@ var Environments = require('./environments');
 module.exports = {
   env: Environments.development,
   server: {
-    logs: true
+    logs: true,
+    ssl: {
+      port: 3001
+    }
   },
   mongo: {
     uri: 'mongodb://localhost/seed_dev'
