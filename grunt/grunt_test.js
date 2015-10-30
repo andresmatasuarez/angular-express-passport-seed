e851c0606471d33c54f10045628845a31e1f2e01
Andrés Mata Suárez <andresmata@zauberlabs.com>
2015-10-30 08:50:38 -0300

    Removed JSHint grunt task

--- grunt/grunt_test.js
+++ grunt/grunt_test.js
@@ -1,28 +1,6 @@
 'use strict';
 
-var jshintStylish  = require('jshint-stylish');
-
 module.exports = function(grunt){
-
-  grunt.config('jshint', {
-    options: { reporter: jshintStylish },
-    assets: {
-      options : { jshintrc: '<%= paths.dev.assets %>/.jshintrc' },
-      src     : [ '<%= paths.dev.assets %>/**/*.js' ]
-    },
-    clientTests: {
-      options : { jshintrc: '<%= paths.tests.client %>/.jshintrc' },
-      src     : [ '<%= paths.tests.client %>/**/*.spec.js' ]
-    },
-    server: {
-      options : { jshintrc: '.jshintrc' },
-      src     : [ '<%= paths.dev.server %>/**/*.js' ]
-    },
-    serverTests: {
-      options : { jshintrc: '<%= paths.tests.server %>/.jshintrc' },
-      src     : [ '<%= paths.tests.server %>/**/*.spec.js' ]
-    }
-  });
 
   grunt.config('mochaTest', {
     options: {
@@ -39,8 +17,8 @@ module.exports = function(grunt){
 
   grunt.registerTask('test', function(target){
     switch(target){
-      case 'server' : return grunt.task.run([ 'env:test',    'jshint:serverTests', 'mochaTest' ]);
-      case 'client' : return grunt.task.run([ 'env:test',    'jshint:clientTests', 'karma' ]);
+      case 'server' : return grunt.task.run([ 'env:test',    'mochaTest' ]);
+      case 'client' : return grunt.task.run([ 'env:test',    'karma' ]);
       default       : return grunt.task.run([ 'test:server', 'test:client' ]);
     }
   });
