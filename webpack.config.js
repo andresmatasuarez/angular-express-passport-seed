3cfdfe513981ad19ed0c2784f691f58ea3b8052b
Andrés Mata Suárez <andresmata@zauberlabs.com>
2015-11-12 19:16:26 -0300

    Removed webpack.config.js from eslint ignore list

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,15 @@
 'use strict';
 
-var FILE_LOADER_NAME = 'name=/[name].[hash].[ext]';
-var FONT_LOADER_NAME = 'name=/[name].[ext]';
+const FILE_LOADER_NAME = 'name=/[name].[hash].[ext]';
+const FONT_LOADER_NAME = 'name=/[name].[ext]';
 
-var path                = require('path');
-var webpack             = require('webpack');
-var AssetsWebpackPlugin = require('assets-webpack-plugin');
+const path                = require('path');
+const webpack             = require('webpack');
+const AssetsWebpackPlugin = require('assets-webpack-plugin');
 
-var resolvePath = function(p){
+function resolvePath(p) {
   return path.resolve(__dirname, p);
-};
+}
 
 module.exports = {
   entry: {
@@ -36,20 +36,20 @@ module.exports = {
       //   /../../partials/_partial.jade --> Matches
       //   ./partials/partial.jade       --> Matches not
       { test: /^\.{0,2}\/?(.+\/)*[^_]+\.jade$/, loader: 'html!jade-html' },
-      { test: /^\.{0,2}\/?(.+\/)*_.+\.jade$/,   loader: 'ngtemplate?relativeTo=' + __dirname + '/client/!html!jade-html' },
+      { test: /^\.{0,2}\/?(.+\/)*_.+\.jade$/,   loader: `ngtemplate?relativeTo=${__dirname}/client/!html!jade-html` },
 
       // Inline base64 URLs for <=8k images, direct URLs for the rest
       {
         test: /\.(jpe?g|png|gif|svg)$/i,
         loaders: [
-          'url?' + FILE_LOADER_NAME + '&limit=8192&hash=sha512&digest=hex',
+          `url?${FILE_LOADER_NAME}&limit=8192&hash=sha512&digest=hex`,
           'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
         ]
       },
 
       // Fonts
-      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,      loader: 'url?' + FONT_LOADER_NAME + '&limit=10000&minetype=application/font-woff' },
-      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file?' + FONT_LOADER_NAME }
+      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,      loader: `url?${FONT_LOADER_NAME}&limit=10000&minetype=application/font-woff` },
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: `file?${FONT_LOADER_NAME}` }
     ]
   },
   resolve: {
