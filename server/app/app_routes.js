7c76ce76bb02fe27aaeac188b27f8ff1c53f4f64
Andrés Mata Suárez <amatasuarez@gmail.com>
2016-02-20 15:09:17 -0300

    Refactored serverBundledIndex into serverBundledView

--- server/app/app_routes.js
+++ server/app/app_routes.js
@@ -10,12 +10,12 @@ const api        = require('../api');
 
 const enforceSSL = RouteUtils.enforceSSL({ port: config.server.ssl.port });
 
-function serveBundledIndex(page, bundleMappingsPath) {
+function serveBundledView(view, page, bundleMappingsPath) {
   return function(req, res, next) {
     fs.readFileAsync(bundleMappingsPath)
     .then(JSON.parse)
     .then(function(mappings) {
-      res.render('index', {
+      res.render(view, {
         settings : config.app[page],
         scripts  : {
           commons : mappings.commons.js,
@@ -48,8 +48,8 @@ module.exports = function(app) {
     index  : false
   }));
 
-  app.use('/dashboard', serveBundledIndex('dashboard', config.app.assets.mappings));
-  app.use('/web',       serveBundledIndex('web',       config.app.assets.mappings));
+  app.use('/dashboard', serveBundledView('index', 'dashboard', config.app.assets.mappings));
+  app.use('/web',       serveBundledView('index', 'web',       config.app.assets.mappings));
 
   // URL rewrite for non-HTML5 browsers
   // Just send the index.html for other files to support HTML5Mode
